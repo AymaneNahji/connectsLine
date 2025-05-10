c465be9f89a76bc0813eab9006535adca188a649
Aymane Nahji <aymanenahjidev@gmail.com>
2025-05-10 13:59:24 +0100

    Sure! Pl

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./html/**/*.html",
+    "./js/**/*.js",
+    "./css/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [],
+} 
